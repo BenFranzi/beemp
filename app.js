84fa094d247a401c13fa4c78506963227c091878
BenFranzi <benjaminfranzi@outlook.com>
2017-01-30 22:19:12 +1100

    basic beemp

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const passport = require('passport')
+const flash = require('connect-flash')
+const session = require('express-session')
+const hbs = require('hbs')
+const app = express()
+const port = process.env.PORT || 3010
+const configDB = require('./app/config/db.js')
+mongoose.connect(configDB.url)
+
+app.use(morgan('dev'))
+app.set('view engine', 'hbs')
+app.use(cookieParser())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// PASSPORT
+app.use(session({
+  secret: 'whenitistimethatswhenyoustrike',
+  resave: 'true',
+  saveUninitialized: 'true'
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
+//
+
+const hbsCfg = require('./app/config/handlebars.js')(hbs)
+const passportCfg = require('./app/config/passport.js')(passport)
+const routesCfg = require('./app/config/routes.js')(app,hbs)
+
+app.listen(port)
+console.log(`Listening on :${port}\n`)
