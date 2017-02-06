451b5806a795feceb24f43b2ae8915b5393c97c4
BenFranzi <benjaminfranzi@outlook.com>
2017-02-06 22:41:55 +1100

    init E001

--- app/config/handlebars.js
+++
@@ -1,9 +0,0 @@
-module.exports = (hbs) => {
-  hbs.registerHelper('if_eq', function(a, b, opts) {
-    if (a == b) {
-        return opts.fn(this);
-    } else {
-        return opts.inverse(this);
-    }
-  })
-}
